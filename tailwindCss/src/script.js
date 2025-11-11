83d3e02061e4e9500a5f5867df1a9502d8dce53c
SaadiaAjbbar <ajbbarsaadia6@gmail.com>
2025-11-11 11:19:25 +0100

    creation des divs cardi avec donnees fetching et header

--- tailwindCss/src/script.js
+++ tailwindCss/src/script.js
@@ -7,15 +7,14 @@ async function getJeux() {
         const data = mydata.results;
         data.forEach(result => {
             const cardi = document.createElement("div");
-            cardi.className = `cardi  border-2 rounded-2xl border-amber-600`;
+            cardi.className = `cardi  border-2 border-amber-600 rounded-2xl w-10`;
            // cardi.style.backgroundImage
             cardi.innerHTML = `
-             <img src='${result.background_image}' alt='backgroundJeu' class=' object-contain'>
+             <img src='${result.background_image}' alt='backgroundJeu' class="object-contain">
              <div class="p-4 text-center ">
                <h3 class="text-lg font-semibold text-white mb-2">${result.name}</h3>
-               <p class="text-sm text-gray-400">${result.rating}⭐</p>
+               <p class="text-sm text-gray-400">Notes:${result.rating}</p>
              </div>
-            
             `;
             cards.appendChild(cardi);
         });
