4795834af580496ed00a7664d18a88f72b33b09b
SaadiaAjbbar <ajbbarsaadia6@gmail.com>
2025-11-13 09:11:05 +0100

    affichage de divs hiden filter , menu ,recherche  acceuil mobile

--- tailwindCss/src/script.js
+++ tailwindCss/src/script.js
@@ -1,4 +1,22 @@
 const cards = document.querySelector(".cards");
+//menu
+function toggleMenu() {
+  const menu = document.getElementById("menuliens");
+  menu.classList.toggle("hidden");
+}
+
+//filter
+function toggleFilter() {
+    const filtre = document.getElementById("filterchoix");
+    filtre.classList.toggle("hidden");
+}
+//recherche
+function RechercheToggle() {
+    const recherch = document.getElementById("RecherchechBar");
+    recherch.classList.toggle("hidden");
+}
+
+//fetch data
 async function getJeux() {
     try {
         const res = await fetch("https://debuggers-games-api.duckdns.org/api/games");
@@ -7,14 +25,19 @@ async function getJeux() {
         const data = mydata.results;
         data.forEach(result => {
             const cardi = document.createElement("div");
-            cardi.className = `cardi  border-2 border-amber-600 rounded-2xl w-10`;
-           // cardi.style.backgroundImage
+            cardi.className = `cardi flex justify-center items-end border-2 border-amber-600 rounded-2xl bg-contain bg-cover bg-no-repeat bg-center w-9/12 h-96`;
+            cardi.style.backgroundImage = `url('${result.background_image}')`;
+
+            const platforms = result.platforms.map(p=>p.platform.name).join(" , ");
             cardi.innerHTML = `
-             <img src='${result.background_image}' alt='backgroundJeu' class="object-contain">
-             <div class="p-4 text-center ">
-               <h3 class="text-lg font-semibold text-white mb-2">${result.name}</h3>
-               <p class="text-sm text-gray-400">Notes:${result.rating}</p>
-             </div>
+              <div class=" p-4 flex flex-col  w-full rounded-b-2xl h-2/5 text-white " style="background-color: rgba(30, 40, 58, 0.8);">
+              <h2 class="text-center font-bold">${result.name}</h2> 
+              <p class="text-sm"><span class="font-bold">Notes:</span> ${result.rating}</p>
+              <p class="text-sm"><span class="font-bold">Plateformes:</span> ${platforms}</p>
+              <button class="hover:bg-white hover:text-amber-500 text-white bg-amber-500 font-semibold py-2 px-6 rounded-3xl">
+               Ajouter au favoris
+              </button>
+              </div>
             `;
             cards.appendChild(cardi);
         });
