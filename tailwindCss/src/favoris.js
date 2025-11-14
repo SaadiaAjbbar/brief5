830053587566254eaa1ffa6758d628bd96f8676a
SaadiaAjbbar <ajbbarsaadia6@gmail.com>
2025-11-14 20:52:26 +0100

    responsive desctop et tablette

--- tailwindCss/src/favoris.js
+++ tailwindCss/src/favoris.js
@@ -1,3 +1,4 @@
+
 const cardsFavoris = document.querySelector(".cards_favoris");
 //menu
 function toggleMenu() {
@@ -23,20 +24,19 @@ function afficherFavoris() {
 
     favoris.forEach(item => {
         const card = document.createElement("div");
-        card.className = "favori_card flex justify-center  items-end border-2 border-amber-600 rounded-2xl bg-contain bg-cover bg-no-repeat bg-center w-9/12 min-h-96 ";
+        card.className = "favori_card flex justify-center  items-end border-2 border-amber-600 rounded-2xl bg-contain bg-cover bg-no-repeat bg-center w-9/12 min-h-96 md:w-2/5 lg:w-[25%]";
         card.style.backgroundImage = `url('${item.image}')`;
-
         card.innerHTML = `
-            <div class="p-4 flex flex-col w-full rounded-b-2xl h-2/5  text-white" style="background-color: rgba(30, 40, 58, 0.8);" 
-                 style="background-color: rgba(30,40,58,0.8);">
-
-                <h2 class="text-center font-bold">${item.name}</h2>
-                <p><strong>Notes :</strong> ${item.rating}</p>
-                <p><strong>Genre :</strong> ${item.genres}</p>
-                <p class="text-sm mt-2">${item.description.slice(0,150)}...</p>
+            <div class="p-4 flex flex-col w-full rounded-b-2xl h-2/5 text-white " style="background-color: rgba(30, 40, 58, 0.8);" 
+              <h2 class="text-center font-bold ">${item.name}</h2>
+               <div class="">
+                <p><strong class="text-amber-500">Notes :</strong> ${item.rating}</p>
+                <p><strong class="text-amber-500">Genre :</strong> ${item.genres}</p>
+               </div>
+                <p class="text-sm mt-2"><strong class="text-amber-500">Description :</strong>${item.description.slice(0,150)}...</p>
 
                 <button data-id="${item.id}" 
-                        class="btn_supprimer bg-amber-500 text-white mt-3 px-4 py-2 rounded">
+                        class="btn_supprimer bg-amber-500 text-white mt-3 px-4 py-2  rounded">
                     Supprimer de favoris
                 </button>
             </div>
@@ -60,4 +60,4 @@ function supprimerFavori(id) {
     afficherFavoris(); // refresh
 }
 
-afficherFavoris();
+afficherFavoris();   
